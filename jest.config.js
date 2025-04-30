621972511d9589fd184ca4198d59bbf05af354d6
Robert Grembowicz <robert.grembowicz@gmail.com>
2025-04-30 21:40:07 +0200

    jest and rtl configuration

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+// jest.config.js
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+};
+
+module.exports = createJestConfig(customJestConfig);
