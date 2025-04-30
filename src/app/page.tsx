71db6966a02080f7179a820fff9514bd48660c33
Robert Grembowicz <robert.grembowicz@gmail.com>
2025-04-30 17:09:38 +0200

    todos and app styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react";
+
 import { fetchTodos } from "@/api/data";
 import { TodoList } from "@/components/TodoList/TodoList";
 
@@ -5,9 +7,19 @@ export default async function Home() {
   const todos = await fetchTodos();
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <TodoList initialTodos={todos} />
+    <div className="flex justify-center font-[family-name:var(--font-geist-sans)] p-4 sm:p-8 h-dvh">
+      <main className="flex lg:max-w-2/3 md:max-w-4/5 shadow-sm p-4 sm:p-8 bg-(--background) rounded-lg">
+        <section aria-labelledby="todo-heading" className="flex flex-col">
+          <h1
+            id="todo-heading"
+            className="text-4xl font-bold text-blue-600 sm:mb-4 text-center p-4 pt-0 sm:p-6 "
+          >
+            TODO&apos;s List
+          </h1>
+          <Suspense fallback={<p>loading...</p>}>
+            <TodoList initialTodos={todos} />
+          </Suspense>
+        </section>
       </main>
     </div>
   );
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -33,19 +33,15 @@ export function TodoList({ initialTodos }: TodosProps) {
   });
 
   return (
-    <section aria-labelledby="todo-heading">
-      <h1 id="todo-heading" className="text-4xl font-bold text-blue-600 mb-4">
-        TODO&apos;s List
-      </h1>
-
-      <div className="mb-4 flex gap-2">
+    <>
+      <div className="mb-4 flex justify-end gap-2">
         {(["all", "completed", "incomplete"] as TFilter[]).map((f) => (
           <button
             key={f}
             onClick={() => setFilter(f)}
             className={`px-3 py-1 rounded border text-sm hover:cursor-pointer ${
               filter === f
-                ? "bg-blue-600 border-blue-600"
+                ? "bg-blue-600 border-blue-600 text-white"
                 : "border-gray-300 hover:bg-gray-100 hover:text-black"
             }`}
           >
@@ -54,16 +50,22 @@ export function TodoList({ initialTodos }: TodosProps) {
         ))}
       </div>
 
-      <ul className="space-y-2" aria-label="Lista zadań">
+      <ul
+        className="space-y-2 overflow-y-scroll overflow-x-hidden"
+        aria-label="Lista zadań"
+      >
         {filtered.map((todo) => (
-          <li key={todo.id} className="flex items-center gap-2">
-            <label className="flex items-center gap-2 cursor-pointer w-full">
+          <li key={todo.id} className="flex items-center gap-2 my-4">
+            <label
+              className="flex items-center
+             gap-2 cursor-pointer w-full"
+            >
               <input
                 type="checkbox"
                 checked={todo.completed}
                 onChange={() => toggleTodo(todo.id)}
                 aria-checked={todo.completed}
-                className="h-5 w-5 text-blue-600 rounded border-gray-300 focus:ring-2 focus:ring-blue-500"
+                className="h-5 w-5 text-blue-600 rounded border-gray-300 focus:ring-2 focus:ring-blue-500 shrink-0"
               />
               <span
                 className={`${
@@ -76,6 +78,6 @@ export function TodoList({ initialTodos }: TodosProps) {
           </li>
         ))}
       </ul>
-    </section>
+    </>
   );
 }
