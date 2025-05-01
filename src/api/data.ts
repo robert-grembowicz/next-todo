075367c317818cb78da6b1d7b91d05ddfa91427a
Robert Grembowicz <robert.grembowicz@gmail.com>
2025-05-01 12:32:05 +0200

    setting delay to 2s

--- src/api/data.ts
+++ src/api/data.ts
@@ -8,7 +8,7 @@ function delay(ms: number) {
 
 export async function fetchTodos(withError?: boolean): Promise<ITodo[]> {
   try {
-    await delay(5000);
+    await delay(2000);
 
     if (withError) {
       throw new Error("Error!!!");
